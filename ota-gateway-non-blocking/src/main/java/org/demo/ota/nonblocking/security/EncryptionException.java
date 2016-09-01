8e26779094f4d5859482008ec22e7dd8a7268b8f
Oleksandr Goldobin <alex.goldobin@gmail.com>
2016-09-01 21:29:17 +0200

    Async HTTP client added to the non-blocking gateway

---
+++ ota-gateway-non-blocking/src/main/java/org/demo/ota/nonblocking/security/EncryptionException.java
@@ -0,0 +1,4 @@
+package org.demo.ota.nonblocking.security;
+
+public class EncryptionException extends RuntimeException {
+}
--- ota-gateway-non-blocking/src/main/java/org/demo/ota/nonblocking/security/SecureModuleClient.java
+++ ota-gateway-non-blocking/src/main/java/org/demo/ota/nonblocking/security/SecureModuleClient.java
@@ -1,24 +1,18 @@
 package org.demo.ota.nonblocking.security;
 
+import org.apache.http.HttpHeaders;
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.DefaultAsyncHttpClient;
+
 import java.util.concurrent.CompletionStage;
 
 public class SecureModuleClient {
 
+    private static final String BASE_URI = "http://localhost:7070/secure-module/encrypt";
+
     private static final SecureModuleClient INSTANCE = new SecureModuleClient();
-//    private Client client;
-//    private final URI secureModuleUri;
 
     private SecureModuleClient() {
-
-//        try {
-//            secureModuleUri = new URI(System.getProperty("secureModuleUri", "http://localhost:7070"));
-//        } catch (URISyntaxException e) {
-//            throw new RuntimeException(e);
-//        }
-
-//        HttpClient httpClient = HttpClientBuilder.create().setConnectionManager(new PoolingHttpClientConnectionManager()).build();
-//        client = new ResteasyClientBuilder().httpEngine(new ApacheHttpClient4Engine(httpClient)).build();
-
     }
 
     public static SecureModuleClient instance() {
@@ -26,20 +20,22 @@ public class SecureModuleClient {
     }
 
     public CompletionStage<String> encrypt(String keyDiversifier, String payload) {
-        throw new UnsupportedOperationException();
-//        WebTarget target = client.target(secureModuleUri).path("/secure-module/encrypt/{keyDiversifier}").resolveTemplate("keyDiversifier", keyDiversifier);
-//
-//        Response response = null;
-//        try {
-//            response = target.request().post(Entity.text(payload));
-//            if (response.getStatus() < 200 || response.getStatus() > 299) {
-//                throw new RuntimeException("Failed HTTP error code " + response.getStatus());
-//            }
-//            return response.readEntity(String.class);
-//        } finally {
-//            if (response != null) {
-//                response.close();
-//            }
-//        }
+
+        final AsyncHttpClient client = new DefaultAsyncHttpClient();
+
+        return client
+        .preparePost(BASE_URI + "/" + keyDiversifier)
+        .setHeader(HttpHeaders.CONTENT_TYPE, "text/plain;charset=UTF-8")
+        .setHeader(HttpHeaders.ACCEPT,  "text/plain")
+        .setBody(payload)
+        .execute()
+        .toCompletableFuture()
+        .thenApply(resp -> {
+           if (resp.getStatusCode() != 200) {
+               throw new EncryptionException();
+           }
+
+           return resp.getResponseBody();
+        });
     }
 }
